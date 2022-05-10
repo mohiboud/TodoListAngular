1e2a660034c94553bcb3de18414e41df533a72ec
mohiboud <99808965+mohiboud@users.noreply.github.com>
2022-05-10 12:08:42 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AllTasksComponent } from './all-tasks/all-tasks.component';
+import { InProgressComponent } from './in-progress/in-progress.component';
+import { DoneComponent } from './done/done.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SignUpComponent,
+    AllTasksComponent,
+    InProgressComponent,
+    DoneComponent
   ],
   imports: [
     BrowserModule,
